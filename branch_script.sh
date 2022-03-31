8fe908a65af6ab9584ab57295dd803129ace0f30
Doud75 <adrienquimbre@gmail.com>
2022-03-31 14:54:33 +0200

    files

---
+++ branch_script.sh
@@ -0,0 +1,8 @@
+`git checkout -b "branch_name1"`
+`git push -u origin "branch_name1"`
+`git checkout -b "branch_name2"`
+`git push -u origin "branch_name2"`
+`git checkout -b "branch_name3"`
+`git push -u origin "branch_name3"`
+`gco main `
+
---
+++ script.sh
@@ -0,0 +1,3 @@
+`git fetch --all -p; git branch -vv | grep gone | awk '{ print $1 }' | tr "\n" "\ " | xargs git branch -D`
+
+# echo $FETCH
